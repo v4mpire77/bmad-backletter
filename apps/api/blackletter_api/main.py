e7445941166558b382fd0e620184fec97e972160
v4mpire77 <Omarrunjanally@gmail.com>
2025-09-02 21:21:12 +0100

    refactor(api): add None return types to ws manager

--- apps/api/blackletter_api/main.py
+++ apps/api/blackletter_api/main.py
@@ -40,17 +40,17 @@ class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket) -> None:
         await websocket.accept()
         self.active_connections.append(websocket)
 
-    def disconnect(self, websocket: WebSocket):
+    def disconnect(self, websocket: WebSocket) -> None:
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
+    async def send_personal_message(self, message: str, websocket: WebSocket) -> None:
         await websocket.send_text(message)
 
-    async def broadcast(self, message: str):
+    async def broadcast(self, message: str) -> None:
         dead_connections: List[WebSocket] = []
         for connection in list(self.active_connections):
             try:
